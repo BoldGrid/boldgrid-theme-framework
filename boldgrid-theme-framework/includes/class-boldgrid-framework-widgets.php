b83f450ed58e77b4622db424c5883b5790375092
Tim <dev@tim.ph>
2016-03-08 13:14:53 -0500

    add alt-font to widget titles

--- boldgrid-theme-framework/includes/class-boldgrid-framework-widgets.php
+++ boldgrid-theme-framework/includes/class-boldgrid-framework-widgets.php
@@ -90,11 +90,11 @@ class BoldGrid_Framework_Widgets {
 			if ( false === $filled ) {
 				$filled_data = "data-empty-area='true'";
 			}
-		
+
 			echo '<div data-widget-area="accordion-section-sidebar-widgets-' .
 				esc_attr( $index ) . '" ' .  esc_attr( $filled_data ) . '>';
 		};
-		
+
 		$after_function = function ( $index, $filled = false ) {
 			echo '</div>';
 		};
@@ -115,7 +115,7 @@ class BoldGrid_Framework_Widgets {
 			'description'   => '',
 			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h1 class="widget-title">',
+			'before_title'  => '<h1 class="widget-title alt-font">',
 			'after_title'   => '</h1>',
 		) );
 	}
--- src/includes/configs/widget.config.php
+++ src/includes/configs/widget.config.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Widget Configuration Options.
+ *
+ * @package Boldgrid_Theme_Framework
+ * @subpackage Boldgrid_Theme_Framework\Configs
+ *
+ * @since    1.1
+ * @return   array   An array of widget configs.
+ */
+
 $year = date( 'Y' );
 $blogname = get_bloginfo( 'name' );
 $custom_widgets['footer-company-details'] = <<<HTML
@@ -7,63 +17,60 @@ HTML;
 
 return array(
 	'footer_widgets' => array(
-		// When the footer is disabled, these widgets will be removed
+		// When the footer is disabled, these widgets will be removed.
 		'boldgrid-widget-3'
 	),
-		'widget_instances' => array(
-			// This supports adding many widgets to 1 area
-			'boldgrid-widget-3' => array(
-				// Specify name as key to assist with lookups
-				'footer-company-details' => array(
-					'title' => 'Contact Info',
-					'text' => $custom_widgets['footer-company-details'],
-					'type' => 'visual',
-					'filter' => 1,
-					'label' => 'black-studio-tinymce',
-				),
+	'widget_instances' => array(
+		// This supports adding many widgets to 1 area.
+		'boldgrid-widget-3' => array(
+			// Specify name as key to assist with lookups.
+			'footer-company-details' => array(
+				'title' => 'Contact Info',
+				'text' => $custom_widgets['footer-company-details'],
+				'type' => 'visual',
+				'filter' => 1,
+				'label' => 'black-studio-tinymce',
 			),
 		),
-		'sidebars' => array(
+	),
+	'sidebars' => array(
 		'boldgrid-widget-1' => array(
 			'name'          => 'Widget 1',
 			'id'            => 'boldgrid-widget-1',
 			'before_widget' => '<aside class="%2$s widget well" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 			'before_bg_widget' => '<aside class="%2$s widget" id="%1$s">',
 			'after_bg_widget' => '</aside>',
 		),
-
 		'boldgrid-widget-2' => array(
 			'name'          => 'Widget 2',
 			'id'            => 'boldgrid-widget-2',
 			'before_widget' => '<aside class="%2$s widget well" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 			'before_bg_widget' => '<aside class="%2$s widget" id="%1$s">',
 			'after_bg_widget' => '</aside>',
 		),
-
 		'boldgrid-widget-3' => array(
 			'name'          => 'Footer Center',
 			'id'            => 'boldgrid-widget-3',
 			'before_widget' => '<aside class="%2$s widget well" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 			'before_bg_widget' => '<aside class="%2$s widget" id="%1$s">',
 			'after_bg_widget' => '</aside>',
 		),
-
-		// Header Widgets
+		// Header Widgets.
 		'header-1' => array(
 			'name'          => __( 'Header Column 1' ),
 			'id'            => 'header-1',
 			'before_widget' => '<aside class="widget well %2$s" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 		),
 		'header-2' => array(
@@ -71,7 +78,7 @@ return array(
 			'id'            => 'header-2',
 			'before_widget' => '<aside class="widget well %2$s" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 		),
 		'header-3' => array(
@@ -79,7 +86,7 @@ return array(
 			'id'            => 'header-3',
 			'before_widget' => '<aside class="widget well %2$s" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 		),
 		'header-4' => array(
@@ -87,18 +94,16 @@ return array(
 			'id'            => 'header-4',
 			'before_widget' => '<aside class="widget well %2$s" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 		),
-
-
-		// Footer Widgets
+		// Footer Widgets.
 		'footer-1' => array(
 			'name'          => __( 'Footer Column 1' ),
 			'id'            => 'footer-1',
 			'before_widget' => '<aside class="%2$s widget well" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 		),
 		'footer-2' => array(
@@ -106,7 +111,7 @@ return array(
 			'id'            => 'footer-2',
 			'before_widget' => '<aside class="%2$s widget well" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 		),
 		'footer-3' => array(
@@ -114,7 +119,7 @@ return array(
 			'id'            => 'footer-3',
 			'before_widget' => '<aside class="%2$s widget well" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 		),
 		'footer-4' => array(
@@ -122,9 +127,9 @@ return array(
 			'id'            => 'footer-4',
 			'before_widget' => '<aside class="%2$s widget well" id="%1$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h2 class="widget-title">',
+			'before_title'  => '<h2 class="widget-title alt-font">',
 			'after_title'   => '</h2>',
 		),
-
-		),
-);?>
+	),
+);
+?>

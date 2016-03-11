3d6fd64c106a46b656d1ac45726b1b57dd9eb133
Tim <dev@tim.ph>
2016-03-11 13:44:24 -0500

    fixing flicker in typography controls.

--- boldgrid-theme-framework/assets/js/customizer/typography-controls.js
+++ boldgrid-theme-framework/assets/js/customizer/typography-controls.js
@@ -1,7 +1,7 @@
 /**
  * We know that this event is triggered by wordpress but we can't get it to work
  * This is a temp solution.
- * 
+ *
  * @param $
  */
 (function( $ ) {
@@ -20,7 +20,7 @@
 			$window.trigger( refresh_event, message );
 			hide_nav_controls();
 			hide_alt_font_controls();
-		} 
+		}
 	} );
 
 	// Set menu location font size's for menu's that are assigned to locations.
@@ -37,21 +37,21 @@
 		// Check all registered menu locations.
 		$menus.forEach( function( id ) {
 			// Deactivate all controls initially
-			wp.customize.control( 'navigation_' + id.themeLocation +'_font_size' ).deactivate();
-			wp.customize.control( 'navigation_' + id.themeLocation +'_font_family' ).deactivate();
-			wp.customize.control( 'navigation_' + id.themeLocation +'_text_transform' ).deactivate();
+			wp.customize.control( 'navigation_' + id.themeLocation +'_font_size' ).deactivate({ duration: 0 });
+			wp.customize.control( 'navigation_' + id.themeLocation +'_font_family' ).deactivate({ duration: 0 });
+			wp.customize.control( 'navigation_' + id.themeLocation +'_text_transform' ).deactivate({ duration: 0 });
 
 			var $menu_selector = wp.customize.previewer.container.find( 'iframe' ).contents()
 				.find( 'div.' + id.themeLocation.replace( /_/g, '-' ) + '-menu:not(:has( ul li.menu-social ) )' );
-				
+
 			// if menus aren't present in the preview, then hide controls.
 			if ( $menu_selector.length ) {
 				// hide relevant font size controls
-				wp.customize.control( 'navigation_' + id.themeLocation +'_font_size' ).activate();
+				wp.customize.control( 'navigation_' + id.themeLocation +'_font_size' ).activate({ duration: 0 });
 				// hide relevant font family controls
-				wp.customize.control( 'navigation_' + id.themeLocation +'_font_family' ).activate();
+				wp.customize.control( 'navigation_' + id.themeLocation +'_font_family' ).activate({ duration: 0 });
 				// hide relevant text transform controls
-				wp.customize.control( 'navigation_' + id.themeLocation +'_text_transform' ).activate();
+				wp.customize.control( 'navigation_' + id.themeLocation +'_text_transform' ).activate({ duration: 0 });
 			}
 		});
 	};
@@ -63,13 +63,13 @@
 		$alt_font = wp.customize.previewer.container
 			.find( 'iframe[title="Site Preview"]' ).last().contents().find( '.alt-font' );
 		if ( ! $alt_font.length ) {
-			wp.customize.control( 'alternate_headings_text_transform' ).deactivate();
-			wp.customize.control( 'alternate_headings_font_size' ).deactivate();
-			wp.customize.control( 'alternate_headings_font_family' ).deactivate();
+			wp.customize.control( 'alternate_headings_text_transform' ).deactivate({ duration: 0 });
+			wp.customize.control( 'alternate_headings_font_size' ).deactivate({ duration: 0 });
+			wp.customize.control( 'alternate_headings_font_family' ).deactivate({ duration: 0 });
 		} else {
-			wp.customize.control( 'alternate_headings_text_transform' ).activate();
-			wp.customize.control( 'alternate_headings_font_size' ).activate();
-			wp.customize.control( 'alternate_headings_font_family' ).activate();
+			wp.customize.control( 'alternate_headings_text_transform' ).activate({ duration: 0 });
+			wp.customize.control( 'alternate_headings_font_size' ).activate({ duration: 0 });
+			wp.customize.control( 'alternate_headings_font_family' ).activate({ duration: 0 });
 		}
 	};
 

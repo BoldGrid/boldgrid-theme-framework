7c96b73d7db0bf7bff42d293e656c1604659c8a1
Tim <dev@tim.ph>
2016-04-27 14:28:54 -0400

    adding skiplink to front-end.js

--- boldgrid-theme-framework/assets/js/front-end.js
+++ boldgrid-theme-framework/assets/js/front-end.js
@@ -17,9 +17,32 @@
 		'common': {
 			init: function() {
 				// JavaScript to be fired on all pages
+				this.skipLink();
 			},
 			finalize: function() {
 				// JavaScript to be fired on all pages, after page specific JS is fired
+			},
+			skipLink: function() {
+				var isWebkit  =  navigator.userAgent.toLowerCase(  ).indexOf( 'webkit' ) > -1,
+				    isOpera   =  navigator.userAgent.toLowerCase(  ).indexOf( 'opera' )  > -1,
+				    isIE      =  navigator.userAgent.toLowerCase(  ).indexOf( 'msie' )   > -1;
+
+				if ( ( isWebkit || isOpera || isIE ) && document.getElementById && window.addEventListener ) {
+					window.addEventListener( 'hashchange', function(  ) {
+						var id = location.hash.substring( 1 ),
+							element;
+						if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
+							return;
+						}
+						element = document.getElementById( id );
+						if ( element ) {
+							if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
+								element.tabIndex = -1;
+							}
+							element.focus();
+						}
+					}, false );
+				}
 			},
 		},
 

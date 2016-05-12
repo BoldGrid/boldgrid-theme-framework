97bec67735fcedf3a29dbd4f0e2e6ca4cf2ff221
BradM <bradm@inmotionhosting.com>
2016-05-12 14:28:20 -0400

    Modify 'edit this page' text within Customizer. (2)

--- boldgrid-theme-framework/includes/partials/customizer-edit.php
+++ boldgrid-theme-framework/includes/partials/customizer-edit.php
@@ -1,8 +1,15 @@
-<div id='entry-content' title='Edit this page' class='dialog-hidden'>
-	This section is part of an individual page of your site. The Customizer (where you are now) is for editing the header and footer or an overall setting of your site like colors and fonts. To edit this section, you'll need to leave the Customizer and go to the Page Editor.
+<?php
+	// Translatable strings for the jQuery UI dialog boxes.
+	$edit_page_header   = __( 'Edit this page', 'bgtfw' );
+	$edit_page_content  = __( "This section is part of an individual page of your site. The Customizer (where you are now) is for editing the header and footer or an overall setting of your site like colors and fonts. To edit this section, you'll need to leave the Customizer and go to the Page Editor.", 'bgtfw' );
+	$edit_title_header  = __( 'Hide page title', 'bgtfw' );
+	$edit_title_content = __( "To hide the title on this page, you'll need to leave the Customizer and go to the Page Editor.", 'bgtfw' );
+?>
+<div id='entry-content' title='<?php echo $edit_page_header ?>' class='dialog-hidden'>
+	<?php echo $edit_page_content; ?>
 </div>
-<div id='entry-title' title='Hide page title' class='dialog-hidden'>
-	To hide the title on this page, you'll need to leave the Customizer and go to the Page Editor.<br />
+<div id='entry-title' title='<?php echo $edit_title_header; ?>' class='dialog-hidden'>
+	<?php echo $edit_title_content; ?><br />
 	<img src='<?php echo bloginfo('stylesheet_directory'); ?>/inc/boldgrid-theme-framework/assets/img/display-page-title.jpg' />
 </div>
 <div id='target-highlight'></div>

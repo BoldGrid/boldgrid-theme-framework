8c0524c1a7ebe9dcc3f9b64a072f0d4ba9b0d2c6
BradM <bradm@inmotionhosting.com>
2016-05-12 14:04:02 -0400

    Typography controls affect jquery ui dialog

--- boldgrid-theme-framework/includes/partials/customizer-edit.php
+++ boldgrid-theme-framework/includes/partials/customizer-edit.php
@@ -1,12 +1,8 @@
 <div id='entry-content' title='Edit this page' class='dialog-hidden'>
-	<p>
-		This section is part of an individual page of your site. The Customizer (where you are now) is for editing the header and footer or an overall setting of your site like colors and fonts. To edit this section, you'll need to leave the Customizer and go to the Page Editor.
-	</p>
+	This section is part of an individual page of your site. The Customizer (where you are now) is for editing the header and footer or an overall setting of your site like colors and fonts. To edit this section, you'll need to leave the Customizer and go to the Page Editor.
 </div>
 <div id='entry-title' title='Hide page title' class='dialog-hidden'>
-	<p>To hide the title on this page, you'll need to leave the Customizer
-		and go to the Page Editor.</p>
-	<img
-		src='<?php echo bloginfo('stylesheet_directory'); ?>/inc/boldgrid-theme-framework/assets/img/display-page-title.jpg' />
+	To hide the title on this page, you'll need to leave the Customizer and go to the Page Editor.<br />
+	<img src='<?php echo bloginfo('stylesheet_directory'); ?>/inc/boldgrid-theme-framework/assets/img/display-page-title.jpg' />
 </div>
 <div id='target-highlight'></div>

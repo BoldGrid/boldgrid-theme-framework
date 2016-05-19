589ac9cab3bcf94e8422066d633c7f9ed05a2c93
Tim <dev@tim.ph>
2016-05-19 10:55:18 -0400

    update gulp build for nicescroll

--- gulpfile.js
+++ gulpfile.js
@@ -89,9 +89,9 @@ gulp.task( 'jsDeps', function(  ) {
 	.pipe( uglify(  ) )
     .pipe( rename({ suffix: '.min' }) )
     .pipe( gulp.dest( config.jsDest + '/offcanvas' ) );
-  // slimscroll
-  gulp.src( config.bower + '/slimScroll/*.js' )
-    .pipe( gulp.dest( config.jsDest + '/slimScroll' ) );
+  // Nicescroll.
+  gulp.src( config.bower + '/jquery.nicescroll/dist/*.{js,png}' )
+    .pipe( gulp.dest( config.jsDest + '/niceScroll' ) );
   // sass.js - Check
   gulp.src( config.bower + '/sass.js/dist/**/*' )
     .pipe( gulp.dest( config.jsDest + '/sass-js' ) );

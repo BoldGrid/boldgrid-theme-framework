a4699ee19260f4a6026d6a5eb65b93f961d72d99
Tim <dev@tim.ph>
2016-08-15 11:11:02 -0400

    add components configs

---
+++ boldgrid-theme-framework/includes/configs/components/buttons.config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Button Configuration Options.
+ *
+ * @package Boldgrid_Theme_Framework
+ * @subpackage Boldgrid_Theme_Framework\Configs
+ *
+ * @since    1.2.3
+ *
+ * @return   array   Array of button configs.
+ */
+
+return array(
+	'enabled' => true,
+);

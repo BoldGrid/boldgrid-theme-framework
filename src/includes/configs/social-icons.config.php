25ae15c87d778631f986446811f957e13e1c9275
Tim <dev@tim.ph>
2016-08-16 20:36:30 -0400

    add social icons config file

---
+++ src/includes/configs/social-icons.config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Social Media Icons as Menu Items preferences.
+ *
+ * @package Boldgrid_Theme_Framework
+ * @subpackage Boldgrid_Theme_Framework\Configs
+ *
+ * @since    1.1
+ *
+ * @return   array   An array of social media icon configs.
+ */
+
+return array(
+	'hide-text'   => true,
+	'size'        => '2x',
+	'type'        => 'icon',
+);

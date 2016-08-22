eb80b513356b08c8c3b89c44b603ae10b71f821c
Tim <dev@tim.ph>
2016-08-22 13:07:26 -0400

    remove configs

--- src/includes/class-boldgrid-framework-activate.php
+++ src/includes/class-boldgrid-framework-activate.php
@@ -192,65 +192,6 @@ class Boldgrid_Framework_Activate {
 	 * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
 	 */
 	public function register_required_plugins() {
-		/*
-		 * Array of plugin arrays. Required keys are name and slug.
-		 * If the source is NOT from the .org repo, then source is also required.
-		 */
-		$plugins = array(
-			array(
-				'name'      => 'BoldGrid Inspirations',
-				'slug'      => 'boldgrid-inspirations',
-				'source'    => 'https://github.com/BoldGrid/boldgrid-inspirations/archive/master.zip',
-				'required'  => true, // If false, the plugin is only 'recommended' instead of required.
-			),
-			array(
-				'name'      => 'BoldGrid Editor',
-				'slug'      => 'boldgrid-editor',
-				'source'    => 'https://github.com/BoldGrid/boldgrid-editor/archive/master.zip',
-				'required'  => true, // If false, the plugin is only 'recommended' instead of required.
-			),
-			array(
-				'name'      => 'BoldGrid Gallery',
-				'slug'      => 'boldgrid-gallery',
-				'source'    => 'https://github.com/BoldGrid/boldgrid-gallery/archive/master.zip',
-				'required'  => false, // If false, the plugin is only 'recommended' instead of required.
-			),
-			array(
-				'name'      => 'BoldGrid SEO',
-				'slug'      => 'boldgrid-seo',
-				'source'    => 'https://github.com/BoldGrid/boldgrid-seo/archive/master.zip',
-				'required'  => false, // If false, the plugin is only 'recommended' instead of required.
-			),
-			array(
-				'name'      => 'BoldGrid Staging',
-				'slug'      => 'boldgrid-staging',
-				'source'    => 'https://github.com/BoldGrid/boldgrid-staging/archive/master.zip',
-				'required'  => false, // If false, the plugin is only 'recommended' instead of required.
-			),
-		);
-
-		/*
-		 * Array of configuration settings. Amend each line as needed.
-		 *
-		 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
-		 * strings available, please help us make TGMPA even better by giving us access to these translations or by
-		 * sending in a pull-request with .po file(s) with the translations.
-		 *
-		 * Only uncomment the strings in the config array if you want to customize the strings.
-		 */
-		$tgm_configs = array(
-			'id'           => 'bgtfw',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-			'default_path' => '',                      // Default absolute path to bundled plugins.
-			'menu'         => 'bgtfw-install-plugins', // Menu slug.
-			'parent_slug'  => 'plugins.php',            // Parent menu slug.
-			'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-			'has_notices'  => true,                    // Show admin notices or not.
-			'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-			'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-			'message'      => '',                      // Message to output right before the plugins table.
-		);
-
 		tgmpa( $plugins, $tgm_configs );
 	}
 }

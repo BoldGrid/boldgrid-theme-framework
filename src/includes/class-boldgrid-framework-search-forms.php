bcf032920630c19432288729274ccfb15268415d
Tim <dev@tim.ph>
2016-09-09 12:13:01 -0400

    remove bootstrap classes from search button

--- src/includes/class-boldgrid-framework-search-forms.php
+++ src/includes/class-boldgrid-framework-search-forms.php
@@ -52,9 +52,9 @@ class BoldGrid_Framework_Search_Forms {
 			<div class="form-group">
 				<input type="text" class="form-control" name="s" value="<?php echo esc_attr( get_search_query( ) ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'bgtfw' ); ?>" />
 			</div>
-			<button type="submit" class="btn btn-default button-primary"><span class="fa fa-search"></span><span>&nbsp;Search</span></button>
+			<button type="submit" class="button-primary"><span class="fa fa-search"></span><span>&nbsp;Search</span></button>
 		</form>
-		
+
 	<?php }
 
 	/**

6c95a8479614ed436c226185a1ef8eae47513d89
Tim <dev@tim.ph>
2016-12-21 00:48:33 -0500

    get singular placeholder for comments

--- src/includes/class-boldgrid-framework-comments.php
+++ src/includes/class-boldgrid-framework-comments.php
@@ -55,9 +55,29 @@ class BoldGrid_Framework_Comments {
 			<div id="comments" class="comments-area">
 				<header>
 					<h2 class="comments-title">
+
 						<?php
-							printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'bgtfw' ),
-							number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+							$comments_number = get_comments_number();
+							if ( 1 === $comments_number ) {
+								printf(
+									/* translators: %s: post title */
+									esc_html_x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'bgtfw' ),
+									'<span>' . get_the_title() . '</span>'
+								);
+							} else {
+								printf( // WPCS: XSS OK.
+									/* translators: 1: number of comments, 2: post title */
+									esc_html( _nx(
+										'%1$s thought on &ldquo;%2$s&rdquo;',
+										'%1$s thoughts on &ldquo;%2$s&rdquo;',
+										$comments_number,
+										'comments title',
+										'bgtfw'
+									) ),
+									number_format_i18n( $comments_number ),
+									'<span>' . get_the_title() . '</span>'
+								);
+							}
 						?>
 					</h2>
 				</header>

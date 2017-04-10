6e18191afbc015dca1f5a99bf2c83aebd341fc5a
Tim <dev@tim.ph>
2017-04-10 14:27:01 -0400

    Product properties should not be accessed directly in wc 3.0

--- src/includes/class-boldgrid-framework-woocommerce.php
+++ src/includes/class-boldgrid-framework-woocommerce.php
@@ -57,7 +57,7 @@ class BoldGrid_Framework_Woocommerce {
 		$link = sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">%s</a>',
 			esc_url( $product->add_to_cart_url() ),
 			esc_attr( isset( $quantity ) ? $quantity : 1 ),
-			esc_attr( $product->id ),
+			esc_attr( $product->get_id() ),
 			esc_attr( $product->get_sku() ),
 			esc_attr( isset( $class ) ? $class : 'btn button-primary' ),
 			esc_html( $product->add_to_cart_text() )

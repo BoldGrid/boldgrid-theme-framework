bda0ae8ccd92ffd9ab654f55023805fd35b92cc2
Tim <dev@tim.ph>
2017-05-14 19:29:29 -0400

    update to current coding standards

--- src/includes/customizer/class-boldgrid-framework-customizer-help-overlay-section.php
+++ src/includes/customizer/class-boldgrid-framework-customizer-help-overlay-section.php
@@ -1,10 +1,17 @@
 <?php
 /**
- * Boldgrid Customizer Help Overlay Section.
+ * Class: Boldgrid_Customizer_Help_Overlay_Section.
  *
- * @since  1.0.0
- * @access public
+ * This is the class responsible for the help overlay customizer section displayed.
+ *
+ * @since      1.4.4
+ * @category   Customizer
+ * @package    Boldgrid_Framework
+ * @subpackage Customizer
+ * @author     BoldGrid <support@boldgrid.com>
+ * @link       https://boldgrid.com
  */
+
 if ( class_exists( 'WP_Customize_Section' ) ) {
 	class Boldgrid_Framework_Customizer_Help_Overlay_Section extends WP_Customize_Section {
 
@@ -24,12 +31,12 @@ if ( class_exists( 'WP_Customize_Section' ) ) {
 		 * @access public
 		 * @return void
 		 */
-		protected function render_template() { ?>
-			<li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }}">
+		protected function render_template() {
+			?><li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }}">
 				<h3 class="accordion-section-title">
 					{{ data.title }}
 				</h3>
-			</li>
-		<?php }
+			</li><?php
+		}
 	}
 }

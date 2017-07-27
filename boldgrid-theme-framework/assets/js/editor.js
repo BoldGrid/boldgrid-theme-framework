9b6f76385c91fdd99ff83bcd416b0a1310df4a44
Rafael Ramos <rramo012@gmail.com>
2017-07-27 11:08:37 -0400

    Append customizer typography styles instead of prepending

--- boldgrid-theme-framework/assets/js/editor.js
+++ boldgrid-theme-framework/assets/js/editor.js
@@ -4,8 +4,13 @@
 	$( function() {
 		tinymce.PluginManager.add( 'boldgrid_theme_framework', function( editor ) {
 			editor.on( 'init', function() {
+				var $style;
+
 				if ( BOLDGRID_THEME_FRAMEWORK.Editor && BOLDGRID_THEME_FRAMEWORK.Editor.mce_inline_styles ) {
-					tinyMCE.activeEditor.dom.addStyle( BOLDGRID_THEME_FRAMEWORK.Editor.mce_inline_styles );
+					$style = $( '<style>' );
+					$style.html( BOLDGRID_THEME_FRAMEWORK.Editor.mce_inline_styles );
+
+					$( tinyMCE.activeEditor.iframeElement ).contents().find( 'head' ).append( $style );
 				}
 			} );
 

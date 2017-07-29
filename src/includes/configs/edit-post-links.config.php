18a8168eb060016a8c67d827ba2e998fb17efeff
Tim <dev@tim.ph>
2017-07-29 01:50:07 -0400

    adding edit post link configs

---
+++ src/includes/configs/edit-post-links.config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Edit Post Links
+ *
+ * @package Boldgrid_Theme_Framework
+ * @subpackage Boldgrid_Theme_Framework\Configs
+ *
+ * @since    1.1
+ *
+ * @return   array   An array of edit post link configurations.
+ */
+
+return array(
+	'enabled'         => true,
+	'useThemeColors'  => false,
+);

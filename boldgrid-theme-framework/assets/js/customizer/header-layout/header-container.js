b1591eb4e0c073fcb4be865ba6d21287f5c7127d
Tim <dev@tim.ph>
2018-02-20 14:36:13 -0500

    account for header position changes

--- boldgrid-theme-framework/assets/js/customizer/header-layout/header-container.js
+++ boldgrid-theme-framework/assets/js/customizer/header-layout/header-container.js
@@ -3,6 +3,19 @@
 	/**
 	 * Update classes for blog layouts.
 	 */
+	wp.customize( 'bgtfw_header_layout_position', function( value ) {
+		value.bind( function( to ) {
+			var navi = $( '#navi' );
+			navi.removeClass( 'container' );
+			if ( to === 'header-top' ) {
+				navi.addClass( wp.customize( 'header_container' )() );
+			}
+		} );
+	} );
+
+	/**
+	 * Update classes for blog layouts.
+	 */
 	wp.customize( 'header_container', function( value ) {
 		value.bind( function( to ) {
 			$( '#navi' ).removeClass( 'container' ).addClass( to );

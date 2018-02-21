73238b9cec9e5f95e97182791f25adbc5ab9cc2f
Tim <dev@tim.ph>
2018-02-21 13:32:15 -0500

    set default to no header image provided

--- src/includes/class-boldgrid-framework-custom-header.php
+++ src/includes/class-boldgrid-framework-custom-header.php
@@ -66,7 +66,7 @@ class Boldgrid_Framework_Custom_Header {
 			apply_filters(
 				'bgtfw_custom_header_args',
 				array(
-					'default-image'      => 'remove-header',
+					'default-image'      => '',
 					'width'              => 2000,
 					'height'             => 1200,
 					'flex-height'        => true,
@@ -80,7 +80,7 @@ class Boldgrid_Framework_Custom_Header {
 		$default_image = get_theme_support( 'custom-header', 'default-image' );
 
 		// If it's not a string 'remove-header', then register the default header image based on main image.
-		if ( $default_image !== 'remove-header' ) {
+		if ( $default_image !== 'remove-header' && ! empty( $default_image ) ) {
 
 			// Get the relative path passed for url and thumbnail_url to use.
 			$default_image = str_replace( get_template_directory_uri(), '', $default_image );
@@ -94,6 +94,8 @@ class Boldgrid_Framework_Custom_Header {
 					),
 				)
 			);
+		} else {
+			register_default_headers( array() );
 		}
 	}
 

5929bcd3ea2f6e67b43e1eb655fdeac4cc3cba98
Tim <dev@tim.ph>
2018-03-19 13:32:15 -0400

    adding inline documentation to starter content class

--- boldgrid-theme-framework/includes/class-boldgrid-framework-starter-content.php
+++ boldgrid-theme-framework/includes/class-boldgrid-framework-starter-content.php
@@ -4,11 +4,11 @@
  *
  * This is used for the starter content functionality in the BoldGrid Theme Framework.
  *
- * @since      2.0.0
- * @category   Customizer
- * @package    Boldgrid_Framework
- * @author     BoldGrid <support@boldgrid.com>
- * @link       https://boldgrid.com
+ * @since    2.0.0
+ * @category Customizer
+ * @package  Boldgrid_Framework
+ * @author   BoldGrid <support@boldgrid.com>
+ * @link     https://boldgrid.com
  */
 
 /**
@@ -23,24 +23,35 @@ class BoldGrid_Framework_Starter_Content {
 	/**
 	 * The BoldGrid Theme Framework configurations.
 	 *
-	 * @since     2.0.0
-	 * @access    protected
-	 * @var       string     $configs       The BoldGrid Theme Framework configurations.
+	 * @since  2.0.0
+	 * @access protected
+	 * @var    string    $configs The BoldGrid Theme Framework configurations.
 	 */
 	protected $configs;
 
 	/**
 	 * Initialize the class and set its properties.
 	 *
-	 * @param     string $configs       The BoldGrid Theme Framework configurations.
-	 * @since     2.0.0
+	 * @since 2.0.0
+	 *
+	 * @param string $configs The BoldGrid Theme Framework configurations.
 	 */
 	public function __construct( $configs ) {
 		$this->configs = $configs;
 	}
 
+	/**
+	 * Adds post meta to get_theme_starter_content filter.
+	 *
+	 * @since  2.0.0
+	 *
+	 * @param  array $content Processed content.
+	 * @param  array $config  Starter content config.
+	 *
+	 * @return array $content Modified $content.
+	 */
 	public function add_post_meta( $content, $config ) {
-		foreach( $config['posts'] as $post => $configs ) {
+		foreach ( $config['posts'] as $post => $configs ) {
 			if ( isset( $configs['meta_input'] ) ) {
 				$content['posts'][ $post ]['meta_input'] = $configs['meta_input'];
 			}
@@ -49,6 +60,12 @@ class BoldGrid_Framework_Starter_Content {
 		return $content;
 	}
 
+	/**
+	 * Declares theme support for starter content using the array of starter
+	 * content found in the BGTFW configurations.
+	 *
+	 * @since 2.0.0
+	 */
 	public function add_theme_support() {
 		if ( ! empty( $this->configs['starter-content'] ) ) {
 			add_theme_support( 'starter-content', $this->configs['starter-content'] );

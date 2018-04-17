3eece334c26fa9375f1d2a4357e9091222e51bd0
BradM <bradm@inmotionhosting.com>
2018-04-17 13:48:36 -0400

    WPB-3762 Various edit button bug fixes

--- src/includes/configs/customizer-options/edit.config.php
+++ src/includes/configs/customizer-options/edit.config.php
@@ -12,56 +12,68 @@
 
 return array(
 	'enabled' => true,
+	'defaultIcon' => 'dashicons-edit',
+	// jQuery selectors to target our hamburger menus.
+	'hamburgers' =>
+		// @since 1.0.0
+		'.navbar-toggle' .
+		// @since 2.0.0
+		', .main-menu-btn',
 	'buttons' => array(
 
 		/*
 		 * An array of general edit buttons.
 		 *
-		 * Settings:
-		 * # icon - A css class that will be added to the button. The default edit button is the
-		 *          pencil icon, 'dashicons-edit'.
+		 * @param icon sring          A css class that will be added to the button. The default edit
+		 *                            button is the pencil icon, 'dashicons-edit'.
+		 * @param isParentColumn bool @since 2.0.0 Whether or not the selector is the parent column.
+		 *                            If it is, then the edit button will be aligned with the selector.
+		 *                            Else, the edit button will be aligned with the element found by
+		 *                            BOLDGRID.CustomizerEdit.parentColumn().
+		 * @param parentColumn string @since 2.0.0 A container to align a button with. In previous versions,
+		 *                            the container was found using BOLDGRID.CustomizerEdit.parentColumn().
+		 *                            That container was generally the closest .col, .row, or .container.
+		 *                            As of 2.0.0, the structure has changed slightly, and for some
+		 *                            elements we need to define the parentColumn. If it's not defined,
+		 *                            it will be determined with the parentColumn() function.
+		 * @param requireText bool    Require that the item we are controling have text. For example,
+		 *                            if we are controlling ".site-title a", we can require that that
+		 *                            element has some sort of text within it, before adding a button
+		 *                            for it.
 		 */
 		'general' => array(
 			array(
 				'control' => 'blogname',
 				'selector' => '.site-title a',
-				'icon' => 'dashicons-edit',
+				'parentColumn' => '.site-title',
+				'requireText' => true,
 			),
 			array(
 				'control' => 'boldgrid_logo_setting',
-				'selector' => '.logo-site-title',
-				'icon' => 'dashicons-edit',
+				'selector' => '.logo-site-title img',
+				'isParentColumn' => true,
 			),
-			array(
-				'control' => 'boldgrid_logo_setting',
-				'selector' => '#main-menu',
-				'icon' => 'dashicons-edit',
-			),
-
 			array(
 				'control' => 'boldgrid_contact_details_setting',
 				'selector' => '.bgtfw.contact-block',
-				'icon' => 'dashicons-edit',
 			),
 			array(
 				'control' => 'hide_boldgrid_attribution',
 				'selector' => '.attribution-theme-mods',
-				'icon' => 'dashicons-edit',
 			),
 			array(
 				'control' => 'entry-content',
 				'selector' => '.entry-content',
-				'icon' => 'dashicons-edit',
+				'parentColumn' => 'main',
 			),
 			array(
 				'control' => 'entry-title',
 				'selector' => '.entry-title',
-				'icon' => 'dashicons-edit',
 			),
 			array(
 				'control' => 'blogdescription',
 				'selector' => '.site-description',
-				'icon' => 'dashicons-edit',
+				'isParentColumn' => true,
 			),
 		),
 	),

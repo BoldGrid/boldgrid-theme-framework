aa8f4d08092a4426a8a87c1eebccda78eceb6d68
Rafael Ramos <rramo012@gmail.com>
2018-05-25 12:24:19 -0400

    Moving dev deps to dev, rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require( 'webpack' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 
 	context: path.resolve( __dirname, 'src' ),
 

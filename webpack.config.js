9a4d69543c465334c3477a800dc19110b6e8c5c4
Rafael Ramos <rramo012@gmail.com>
2018-05-30 15:01:22 -0400

    Reset to a production build setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require( 'path' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 
 	context: path.resolve( __dirname, 'src' ),
 

6d3b15c66a1309f13a68bf4b8d1f79617c1a58f8
Rafael Ramos <rramo012@gmail.com>
2018-06-01 10:06:30 -0400

    Build

---
+++ boldgrid-theme-framework/assets/js/customizer/generic/border.js
@@ -0,0 +1,106 @@
+import { PaletteSelector } from '../color/palette-selector';
+import { Border as BaseBorder } from '@boldgrid/controls/src/controls/border';
+
+const $ = window.jQuery;
+
+export class Border extends BaseBorder {
+
+	/**
+	 * Setup the color control.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param {object} options   Options for the parent control.
+	 * @param {object} wpControl A WordPress Control.
+	 */
+	constructor( options, wpControl ) {
+		super( options );
+
+		this.paletteSelector = new PaletteSelector();
+		this.wpControl = wpControl;
+		this.colorControl = wp.customize.control( wpControl.id + '_color' );
+	}
+
+	/**
+	 * Override the render method, to bind events for the border color.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @return {jQuery} jQuery Control.
+	 */
+	render() {
+		let $control = super.render();
+
+		// If an associated color control is found bind events for it.
+		if ( this.colorControl ) {
+			this.toggleVisibility( this.getSettings() );
+			this._bindBorderColor();
+			this._bindParentChange();
+			this._bindCustomizerRefresh();
+		}
+
+		return $control;
+	}
+
+	/**
+	 * Update the visibility of the color control.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param  {object} settings Settings.
+	 * @return {boolean}         Was the control state changed.
+	 */
+	toggleVisibility( settings ) {
+		return ! settings.type ? this.colorControl.deactivate() : this.colorControl.activate();
+	}
+
+	/**
+	 * Override the getting settings call to append the border color.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @return {object} Settings.
+	 */
+	getSettings() {
+		let settings = super.getSettings();
+
+		if ( this.colorControl ) {
+			let valueValue = this.paletteSelector.getColor( this.colorControl.setting() ),
+			colorCSS = this.controlOptions.control.selectors.join( ',' ) +
+				'{border-color:' + valueValue + '}';
+
+			settings.css += colorCSS;
+		}
+
+		return settings;
+	}
+
+	/**
+	 * When the customizer is refreshed update the visibility of the color control.
+	 *
+	 * @since 2.0.0
+	 */
+	_bindCustomizerRefresh() {
+		$( window ).on( 'boldgrid_customizer_refresh', () => {
+			this.toggleVisibility( this.getSettings() );
+		} );
+	}
+
+	/**
+	 * When the color changes, trigger a change of the width.
+	 *
+	 * @since 2.0.0
+	 */
+	_bindBorderColor() {
+		this.colorControl.setting.bind( () => this.events.emit( 'change', this.getSettings() ) );
+	}
+
+	/**
+	 * When the border style/width changes add color.
+	 *
+	 * @since 2.0.0
+	 */
+	_bindParentChange() {
+		this.events.on( 'change', ( settings ) => this.toggleVisibility( settings ) );
+	}
+}

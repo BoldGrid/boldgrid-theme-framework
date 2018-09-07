f720d4da2e45616e8b0019ac2d2d7807f8813c3e
Tim <dev@tim.ph>
2018-09-07 17:39:47 -0400

    allow category and author pages to inherit page title settings

--- src/includes/configs/customizer/controls.config.php
+++ src/includes/configs/customizer/controls.config.php
@@ -423,6 +423,17 @@ return array(
 								esc_url( get_permalink( get_option( 'page_for_posts', true ) ) ),
 								get_the_title( get_option( 'page_for_posts', true ) )
 							);
+						} else if ( is_archive() || is_author() ) {
+							$queried_obj_id = get_queried_object_id();
+							$archive_url = is_author() ? get_author_posts_url( $queried_obj_id ) : get_term_link( $queried_obj_id );
+							printf(
+								'<p class="page-title %1$s"><a %2$s href="%3$s" rel="bookmark">%4$s</a></p>',
+								get_theme_mod( 'bgtfw_pages_title_size' ),
+								BoldGrid::add_class( 'pages_title', [ 'link' ], false ),
+								esc_url( $archive_url ),
+								get_the_archive_title()
+							);
+							the_archive_description( '<div class="taxonomy-description">', '</div>' );
 						} else {
 							the_title( sprintf( '<p class="entry-title page-title ' . get_theme_mod( 'bgtfw_pages_title_size' ) . '"><a ' . BoldGrid::add_class( 'pages_title', [ 'link' ], false ) . ' href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></p>' );
 						}
@@ -503,7 +514,7 @@ return array(
 		},
 		'output' => array(
 			array(
-				'element'  => '.page .entry-header .entry-title, .blog .page-header .page-title, .archive .page-header .page-title',
+				'element'  => '.page .entry-header .entry-title, .blog .page-header .page-title, .archive .page-header',
 				'property' => 'text-align',
 			),
 		),

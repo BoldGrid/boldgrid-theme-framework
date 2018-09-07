62dacc3b13fce63f61ca7424dc573d88c72439bc
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2018-09-07 09:05:55 -0400

    BGTHEME-459 Prevent interaction with panels until customizer loaded. boldgrid-theme-framework/ files only

--- boldgrid-theme-framework/assets/js/customizer/controls.js
+++ boldgrid-theme-framework/assets/js/customizer/controls.js
@@ -5,7 +5,9 @@
  * @param $
  */
 ( function( $ ) {
-	var refreshEvent = $.Event( 'boldgrid_customizer_refresh' );
+	var refreshEvent = $.Event( 'boldgrid_customizer_refresh' ),
+		$body = $( 'body' ),
+		$themeControls = $( '#customize-theme-controls' );
 	$window = $( window );
 
 	$window.on( 'message', function( e ) {
@@ -22,8 +24,19 @@
 			$window.trigger( refreshEvent, message );
 		}
 	} );
+	
+	// Prevent interaction with panels until Customizer fully loads.
+	$body.addClass( 'pre-initial-refresh' );
+	$themeControls.prop( 'title', BOLDGRID.CUSTOMIZER.data.loadingTitle );
 
 	$( function() {
+		
+		// Customizer has fully loaded. Allow for interaction.
+		$( window ).one( 'boldgrid_customizer_refresh', function() {
+			$body.removeClass( 'pre-initial-refresh' );
+			$themeControls.removeAttr( 'title', '' );
+		});
+		
 		$( document ).on( 'click', '.open-widgets-section', function() {
 			wp.customize.panel( 'widgets' ).focus();
 		} );

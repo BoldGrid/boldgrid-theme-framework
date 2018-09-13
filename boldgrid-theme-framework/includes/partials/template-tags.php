58b9834c6ee1f7da64d128ca2eb29aa7d48b96cf
Tim <dev@tim.ph>
2018-09-13 19:44:10 -0400

    fixing build

--- boldgrid-theme-framework/includes/partials/template-tags.php
+++ boldgrid-theme-framework/includes/partials/template-tags.php
@@ -496,7 +496,6 @@ function bgtfw_get_featured_img_bg( $post_id, $theme_mod = false ) {
 		$style = 'style="background: ' . $color . 'url(' . $img[0] . '); background-size: cover; background-position: center center;"';
 	}
 
-
 	return $style;
 }
 

ef3a7ac56d257cc150a85f9a3acd46434cacca2a
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2018-09-20 14:54:29 -0400

    Welcome page verbiage change

--- src/includes/partials/welcome.php
+++ src/includes/partials/welcome.php
@@ -14,6 +14,9 @@ if ( ! defined( 'WPINC' ) ) {
 $allowed_html = array(
 	'a' => array(
 		'href' => array(),
+		'target' => array(
+			'blank',
+		),
 	),
 );
 
@@ -21,6 +24,8 @@ $allowed_html = array(
 $tgm_url = admin_url( 'admin.php?page=bgtfw-install-plugins' );
 
 $customizer_url = admin_url( 'customize.php' );
+
+$registraton_url = admin_url( 'admin.php?page=boldgrid-connect.php' );
 
 global $boldgrid_theme_framework;
 $configs = $boldgrid_theme_framework->get_configs();
@@ -44,15 +49,25 @@ $configs = $boldgrid_theme_framework->get_configs();
 		<div class="welcome-panel-content">
 			<div class="welcome-panel-column-container two-col">
 				<div class="welcome-panel-column">
-					<h2><?php esc_html_e( 'Register Your Account With BoldGrid Central', 'bgtfw' ); ?></h2>
-					<p><?php esc_html_e( 'BoldGrid Central will allow you to generate your connect key, which will unlock all the features of your BoldGrid Crio purchase. Central also allows you to submit personalized support requests.', 'bgtfw' ); ?></p>
-					<p class=>
-						<a href="https://www.boldgrid.com/central/code/envato" class="button button-primary button-hero"><?php esc_html_e( 'Register', 'bgtfw' ); ?></a>
+					<h2><?php esc_html_e( 'Register and Verify Your Purchase', 'bgtfw' ); ?></h2>
+					<p><?php esc_html_e( 'To claim your Premium Connect Key we need to verify your Envato purchase and setup your BoldGrid Central account.  To do this automatically we need access to read the purchase record in your Envato Account.  You can also do this manually by signing up for BoldGrid Central and providing your Envato purchase code to our Support Team in a ticket.', 'bgtfw' ); ?></p>
+					<p>
+						<a href="https://www.boldgrid.com/central/code/envato" class="button button-primary button-hero" target="_blank"><?php esc_html_e( 'Envato Auto Verify', 'bgtfw' ); ?></a>
 						<span class="nowrap">
 							<?php esc_html_e( 'or', 'bgtfw' ); ?>
-							<a href="https://www.boldgrid.com/central"><?php esc_html_e( 'Login to BoldGrid Central', 'bgtfw' ); ?></a>
+							<a href="https://www.boldgrid.com/central" target="_blank"><?php esc_html_e( 'Manually Setup and Verify', 'bgtfw' ); ?></a>
 						</span>
 					</p>
+					<p><?php
+						printf(
+							wp_kses(
+								/* translators: The Url to the Post and Page Builder on boldgrid.com */
+								__( 'Have your Connect Key? Go to <a href="%1$s">Registration</a>.', 'bgtfw' ),
+								$allowed_html
+							),
+							esc_url( $registraton_url )
+						);
+					?></p>
 				</div>
 				<div class="welcome-panel-column">
 					<img style="width:100%;" src="<?php echo esc_url( $configs['framework']['admin_asset_dir'] . 'img/welcome/bg-central.png' ); ?>" />
@@ -71,7 +86,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 						printf(
 							wp_kses(
 								/* translators: The Url to the Post and Page Builder on boldgrid.com */
-								__( 'The <a href="%1$s">BoldGrid Page and Post Builder</a> provides a true WYSIWYG experience while allowing full control over your content. Easily try out new icons, section backgrounds, column and row designs, text settings and more. You can even customize our preset icons, images, and backgrounds within the editor to make them your own.', 'bgtfw' ),
+								__( 'The <a href="%1$s" target="_blank">BoldGrid Page and Post Builder</a> provides a true WYSIWYG experience while allowing full control over your content. Easily try out new icons, section backgrounds, column and row designs, text settings and more. You can even customize our preset icons, images, and backgrounds within the editor to make them your own.', 'bgtfw' ),
 								$allowed_html
 							),
 							esc_url( 'https://www.boldgrid.com/wordpress-page-builder-by-boldgrid/' )
@@ -82,7 +97,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 						<a href="<?php echo esc_url( $tgm_url ); ?>" class="button button-primary button-hero"><?php esc_html_e( 'Activate', 'bgtfw' ); ?></a>
 						<span class="nowrap">
 							<?php esc_html_e( 'or', 'bgtfw' ); ?>
-							<a href="https://www.boldgrid.com/support/post-page-builder-plugin"><?php esc_html_e( 'See Support Documents...', 'bgtfw' ); ?></a>
+							<a href="https://www.boldgrid.com/support/post-page-builder-plugin" target="_blank"><?php esc_html_e( 'See Support Documents...', 'bgtfw' ); ?></a>
 						</span>
 					</p>
 				</div>
@@ -129,7 +144,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 						<a href="<?php echo esc_url( $customizer_url ); ?>" class="button button-primary button-hero"><?php esc_html_e( 'Get Started', 'bgtfw' ); ?></a>
 						<span class="nowrap">
 							<?php esc_html_e( 'or', 'bgtfw' ); ?>
-							<a href="https://www.boldgrid.com/support/"><?php esc_html_e( 'See Support Documents...', 'bgtfw' ); ?></a>
+							<a href="https://www.boldgrid.com/support/" target="_blank"><?php esc_html_e( 'See Support Documents...', 'bgtfw' ); ?></a>
 						</span>
 					</p>
 				</div>
@@ -144,12 +159,12 @@ $configs = $boldgrid_theme_framework->get_configs();
 		<div class="welcome-panel col">
 			<h2><?php esc_html_e( 'Feature Requests and Bug Reports', 'bgtfw' ); ?></h2>
 			<p><?php esc_html_e( 'Help us make BoldGrid Crio better. To submit your product ideas or feedback about current features, or to report bugs and issues, please visit:', 'bgtfw' ); ?></p>
-			<p><a href="https://boldgrid.com/feedback/" class="button button-primary button-hero"><?php esc_html_e( 'BoldGrid Community Portal', 'bgtfw' ); ?></a></p>
+			<p><a href="https://boldgrid.com/feedback/" target="_blank" class="button button-primary button-hero"><?php esc_html_e( 'BoldGrid Community Portal', 'bgtfw' ); ?></a></p>
 		</div>
 		<div class="welcome-panel col">
 			<h2><?php esc_html_e( '1 on 1 Support', 'bgtfw' ); ?></h2>
 			<p><?php esc_html_e( 'Need help with BoldGrid Crio? Contact a member of our knowledgable support team:', 'bgtfw' ); ?></p>
-			<p><a href="https://www.boldgrid.com/central" class="button button-primary button-hero"><?php esc_html_e( 'BoldGrid Central', 'bgtfw' ); ?></a></p>
+			<p><a href="https://www.boldgrid.com/central" target="_blank" class="button button-primary button-hero"><?php esc_html_e( 'BoldGrid Central', 'bgtfw' ); ?></a></p>
 		</div>
 	</div>
 
@@ -167,7 +182,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 						printf(
 							wp_kses(
 								/* translators: URL to boldgrid backup on boldgrid.com. */
-								__( '<a href="%1$s">BoldGrid Backup</a> will backup your entire WordPress site with just a couple of clicks right in your WordPress dashboard. Just select a time and day for backups to run automatically. Or, manually create a backup at any time with a single click. You can also create offsite backups at Amazon S3 or a web server of your choosing.', 'bgtfw' ),
+								__( '<a href="%1$s" target="blank">BoldGrid Backup</a> will backup your entire WordPress site with just a couple of clicks right in your WordPress dashboard. Just select a time and day for backups to run automatically. Or, manually create a backup at any time with a single click. You can also create offsite backups at Amazon S3 or a web server of your choosing.', 'bgtfw' ),
 								$allowed_html
 							),
 							esc_url( 'https://www.boldgrid.com/wordpress-backup-plugin/' )
@@ -178,7 +193,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 						<a href="<?php echo esc_url( $tgm_url ); ?>" class="button button-primary button-hero"><?php esc_html_e( 'Install', 'bgtfw' ); ?></a>
 						<span class="nowrap">
 							<?php esc_html_e( 'or', 'bgtfw' ); ?>
-							<a href="https://www.boldgrid.com/support/backup-plugin"><?php esc_html_e( 'See Support Documents...', 'bgtfw' ); ?></a>
+							<a href="https://www.boldgrid.com/support/backup-plugin" target="_blank"><?php esc_html_e( 'See Support Documents...', 'bgtfw' ); ?></a>
 						</span>
 					</p>
 				</div>
@@ -199,7 +214,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 						printf(
 							wp_kses(
 								/* translators: url to Easy SEO plugin on boldgrid.com. */
-								__( '<a href="%1$s">BoldGrid SEO</a> analyzes your page content in real-time and makes recommendations to help you maintain best SEO practices while writing content. Just set your target keyword or phrase and the BoldGrid SEO dashboard will update your stats on all important on-page SEO factors.', 'bgtfw' ),
+								__( '<a href="%1$s" target="_blank">BoldGrid SEO</a> analyzes your page content in real-time and makes recommendations to help you maintain best SEO practices while writing content. Just set your target keyword or phrase and the BoldGrid SEO dashboard will update your stats on all important on-page SEO factors.', 'bgtfw' ),
 								$allowed_html
 							),
 							esc_url( 'https://www.boldgrid.com/wordpress-seo-plugin/' )
@@ -210,7 +225,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 						<a href="<?php echo esc_url( $tgm_url ); ?>" class="button button-primary button-hero"><?php esc_html_e( 'Install', 'bgtfw' ); ?></a>
 						<span class="nowrap">
 							<?php esc_html_e( 'or', 'bgtfw' ); ?>
-							<a href="https://www.boldgrid.com/support/seo-plugin"><?php esc_html_e( 'See Support Documents...', 'bgtfw' ); ?></a>
+							<a href="https://www.boldgrid.com/support/seo-plugin" target="_blank"><?php esc_html_e( 'See Support Documents...', 'bgtfw' ); ?></a>
 						</span>
 					</p>
 				</div>

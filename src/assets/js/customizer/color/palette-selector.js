cc13783bd299cc759adff7d26d1932bfb041e002
Tim <dev@tim.ph>
2018-10-14 21:36:03 -0400

    allow optional param for getting CSS variable or actual color on palette.getColor

--- src/assets/js/customizer/color/palette-selector.js
+++ src/assets/js/customizer/color/palette-selector.js
@@ -5,12 +5,40 @@ export class PaletteSelector {
 	 *
 	 * @since 2.0.0
 	 *
-	 * @param  {string} themeMod Theme mod value.
-	 * @return {string}       HEX, RGB, RGBA value.
+	 * @param  {string}  themeMod Theme mod value.
+	 * @param  {Boolean} variable Whether to return color variable or true color value.
+	 *
+	 * @return {string}           CSS Color Variable, HEX, RGB, RGBA or value.
 	 */
-	getColor( themeMod ) {
+	getColor( themeMod, variable = false ) {
+		return variable ? this.getVariableValue( themeMod ) : this.getColorVariable( themeMod );
+	}
+
+	/**
+	 * Get the CSS variable for the palette selector control.
+	 *
+	 * @since 2.0.1
+	 *
+	 * @param  {string}  themeMod Theme mod value.
+	 *
+	 * @return {string}           CSS Color Variable.
+	 */
+	getColorVariable( themeMod ) {
 		let colorNumber = this.getColorNumber( themeMod );
 		return `var(--${colorNumber});`;
+	}
+
+	/**
+	 * Get the color value for the palette selector control.
+	 *
+	 * @since 2.0.1
+	 *
+	 * @param  {string}  themeMod Theme mod value.
+	 *
+	 * @return {string}           CSS Color Variable.
+	 */
+	getVariableValue( themeMod ) {
+		return themeMod.split( ':' ).pop();
 	}
 
 	/**
@@ -18,7 +46,7 @@ export class PaletteSelector {
 	 *
 	 * @since 2.0.0
 	 *
-	 * @param  {string} themeMod Theme mod value.
+	 * @param {string} themeMod Theme mod value.
 	 * @return {string} Color number.
 	 */
 	getColorNumber( themeMod ) {

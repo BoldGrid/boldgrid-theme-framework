5e596cf81d79278fb613f1336a698bb5f0e88a32
Tim <dev@tim.ph>
2018-10-23 12:23:50 -0400

    initial header-layout change

---
+++ src/includes/customizer/class-boldgrid-framework-customizer-control-sortable-accordion.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Class: Boldgrid_Framework_Customizer_Control_Sortable_Accordion
+ *
+ * This class is responsible for creating the sortable accordion
+ * controls in the WordPress customizer.
+ *
+ * @since      2.0.0
+ * @category   Customizer
+ * @package    Boldgrid_Framework
+ * @author     BoldGrid <support@boldgrid.com>
+ * @link       https://boldgrid.com
+ */
+if ( class_exists( 'WP_Customize_Control' ) ) {
+
+	class Boldgrid_Framework_Customizer_Control_Sortable_Accordion extends WP_Customize_Control {
+
+		/**
+		 * The type of control being rendered
+		 */
+		public $type = 'bgtfw-sortable-accordion';
+
+		public $items;
+
+		/**
+		 * Refresh the parameters passed to the JavaScript via JSON.
+		 *
+		 * @since 2.0.0
+		 */
+		public function to_json() {
+
+			// Call parent to_json() method to get the core defaults like "label", "description", etc.
+			parent::to_json();
+
+			// ID.
+			$this->json['id'] = $this->id;
+
+			// The setting value.
+			$this->json['value'] = $this->value();
+
+			// The data link.
+			$this->json['link'] = $this->get_link();
+
+			// Addable Item Types.
+			$this->json['items'] = $this->items;
+		}
+
+		/**
+		 * An Underscore (JS) template for this control's content (but not its container).
+		 *
+		 * Class variables for this control class are available in the `data` JS object;
+		 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
+		 *
+		 * @see WP_Customize_Control::print_template()
+		 *
+		 * @access protected
+		 */
+		protected function content_template() {
+			?>
+			<div class="{{ data.type }}">
+				<# if ( data.label ) { #>
+					<span class="customize-control-title">{{{ data.label }}}</span>
+				<# } #>
+				<# if ( data.description ) { #>
+					<span class="description customize-control-description">{{{ data.description }}}</span>
+				<# } #>
+				<input type="hidden" id="input-{{ data.id }}" name="{{ data.id }}" value="<# JSON.stringify( data.value ) #>" class="customize-control-drag-and-drop" {{{ data.link }}} />
+				<div id="sortable-{{ data.id }}">
+					<# _.each( data.value, function( repeaters, sortable ) { #>
+						<div id="sortable-{{ sortable }}-wrapper" class="sortable-wrapper">
+							<span class="sortable-title"><?php esc_html_e( 'Section', 'bgtfw' ); ?></span>
+							<div class="sortable-accordion-content">
+								<div class="sortable-section-controls">
+									<div class="bgtfw-container-control">
+										<# var selected = repeaters.container === 'container' ? 'selected' : ''; #>
+										<div class="bgtfw-sortable-control container {{ selected }}" data-container="container">
+											<span class="bgtfw-icon icon-layout-container"></span>
+											<span><?php esc_html_e( 'Container', 'bgtfw' ); ?></span>
+										</div>
+										<# var selected = repeaters.container !== 'container' ? 'selected' : ''; #>
+										<div class="bgtfw-sortable-control full-screen {{ selected }}" data-container="full-width">
+											<span class="bgtfw-icon icon-layout-full-screen"></span>
+											<span><?php esc_html_e( 'Full Width', 'bgtfw' ); ?></span>
+										</div>
+									</div>
+									<div class="bgtfw-column-control">
+										<div class="row resizable-columns">
+										<# _.each( repeaters.items, function( repeater ) { #>
+											<div class="col-sm-{{ repeater.columnWidth }} resizable" data-type="{{ repeater.type }}">
+												<h3 style="font-size: 14px;">{{{ repeater.columnWidth }}}</h3>
+											</div>
+										<# } ); #>
+										</div>
+									</div>
+								</div>
+								<ul id="sortable-{{ sortable }}" class="connected-sortable">
+								<# _.each( repeaters.items, function( repeater, i ) { #>
+									<li class="repeater" data-value="{{ repeater.type }}">
+										<div class="repeater-input">
+											<span class="repeater-title">{{{ repeater.type }}}</span>
+											<div class="repeater-accordion-content">
+												{{{ repeater.type }}}
+											</div>
+										</div>
+									</li>
+								<# } ); #>
+								</ul>
+								<div class="sortable-actions"></div>
+							</div>
+						</div>
+					<# } ); #>
+				</div>
+			</div>
+			<?php
+		}
+	}
+}

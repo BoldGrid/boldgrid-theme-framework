3aff7e34039aa4a582b497d2c1ce317aa526b164
Tim <dev@tim.ph>
2018-10-30 12:27:24 -0400

    add wrapper hook into attribution action

--- boldgrid-theme-framework/includes/customizer/class-boldgrid-framework-customizer-footer.php
+++ boldgrid-theme-framework/includes/customizer/class-boldgrid-framework-customizer-footer.php
@@ -34,47 +34,51 @@ class Boldgrid_Framework_Customizer_Footer {
 	 *  @since     1.0.0
 	 */
 	public function attribution_display_action() {
-
-		$theme_mods = '';
-
-		// BoldGrid.com Link.
-		if ( ! get_theme_mod( 'hide_boldgrid_attribution' ) ) {
-			$theme_mods .= sprintf(
-				'<span class="link boldgrid-attribution-link">%s <a href="%s" rel="nofollow" target="_blank">%s</a></span>',
-				__( 'Built with', 'bgtfw' ),
-				'http://boldgrid.com/',
-				__( 'BoldGrid', 'bgtfw' )
-			);
-		}
-
-		// WordPress.org Link.
-		if ( ! get_theme_mod( 'hide_wordpress_attribution' ) ) {
-			$theme_mods .= sprintf(
-				'<span class="link wordpress-attribution-link">%s <a href="%s" rel="nofollow" target="_blank">%s</a></span>',
-				__( 'Powered by', 'bgtfw' ),
-				'https://wordpress.org/',
-				__( 'WordPress', 'bgtfw' )
-			);
-		}
-
-		// Allow plugins or themes to add additional attribution links to footer.
-		$additional_links = '';
-		$additional_links = apply_filters( 'bgtfw_attribution_links', $additional_links );
-		$theme_mods .= $additional_links;
-
-		$allowed = [
-			'a' => [
-				'href' => [],
-				'title' => [],
-				'rel' => [],
-				'target' => [],
-			],
-			'span' => [
-				'class' => [],
-			],
-		];
 		?>
-		<span class="attribution-theme-mods"><?php echo wp_kses( $theme_mods, $allowed ); ?></span>
+		<div <?php BoldGrid::add_class( 'attribution_theme_mods_wrapper', [ 'attribution-theme-mods-wrapper' ] ); ?>>
+		<?php
+
+			$theme_mods = '';
+
+			// BoldGrid.com Link.
+			if ( ! get_theme_mod( 'hide_boldgrid_attribution' ) ) {
+				$theme_mods .= sprintf(
+					'<span class="link boldgrid-attribution-link">%s <a href="%s" rel="nofollow" target="_blank">%s</a></span>',
+					__( 'Built with', 'bgtfw' ),
+					'http://boldgrid.com/',
+					__( 'BoldGrid', 'bgtfw' )
+				);
+			}
+
+			// WordPress.org Link.
+			if ( ! get_theme_mod( 'hide_wordpress_attribution' ) ) {
+				$theme_mods .= sprintf(
+					'<span class="link wordpress-attribution-link">%s <a href="%s" rel="nofollow" target="_blank">%s</a></span>',
+					__( 'Powered by', 'bgtfw' ),
+					'https://wordpress.org/',
+					__( 'WordPress', 'bgtfw' )
+				);
+			}
+
+			// Allow plugins or themes to add additional attribution links to footer.
+			$additional_links = '';
+			$additional_links = apply_filters( 'bgtfw_attribution_links', $additional_links );
+			$theme_mods .= $additional_links;
+
+			$allowed = [
+				'a' => [
+					'href' => [],
+					'title' => [],
+					'rel' => [],
+					'target' => [],
+				],
+				'span' => [
+					'class' => [],
+				],
+			];
+			?>
+			<span class="attribution-theme-mods"><?php echo wp_kses( $theme_mods, $allowed ); ?></span>
+		</div>
 		<?php
 	}
 }

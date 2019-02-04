6e491f7bf81f026817f8b9260f55fca2bfeba344
Tim <dev@tim.ph>
2019-02-04 16:53:55 -0500

    add icons for all panel configs

--- src/includes/configs/customizer/panels.config.php
+++ src/includes/configs/customizer/panels.config.php
@@ -20,43 +20,51 @@ return array(
 		'title' => __( 'Fonts', 'bgtfw' ),
 		'description' => '<div class="bgtfw-description"><p>' . esc_html__( 'Manage your site\'s typography settings.', 'bgtfw' ) . '</p><div class="help"><a href="https://www.boldgrid.com/support/boldgrid-crio/working-with-fonts-in-boldgrid-crio/" target="_blank"><span class="dashicons"></span>Help</a></div></div>',
 		'priority' => 90,
+		'icon' => 'dashicons-editor-textcolor',
 	),
 	'bgtfw_header' => array(
 		'title' => __( 'Header', 'bgtfw' ),
 		'priority' => 1,
 		'panel' => 'bgtfw_design_panel',
+		'icon' => 'icon-header-settings',
 	),
 	'bgtfw_site_content' => array(
 		'title' => __( 'Site Content', 'bgtfw' ),
 		'priority' => 2,
 		'panel' => 'bgtfw_design_panel',
+		'icon' => 'dashicons-welcome-widgets-menus',
 	),
 	'bgtfw_footer' => array(
 		'title' => __( 'Footer', 'bgtfw' ),
 		'priority' => 3,
 		'panel' => 'bgtfw_design_panel',
+		'icon' => 'icon-footer-settings',
 	),
 	'bgtfw_blog_panel' => array(
 		'title' => __( 'Blog', 'bgtfw' ),
 		'priority' => 5,
 		'panel' => 'bgtfw_design_panel',
+		'icon' => 'dashicons-admin-post',
 	),
 	'bgtfw_blog_blog_page_panel' => array(
 		'title' => __( 'Blog Page', 'bgtfw' ),
 		'description' => '<div class="bgtfw-description"><p>' . esc_html__( 'Change the display of your site\'s blog page.', 'bgtfw' ) . '</p><div class="help"><a href="https://www.boldgrid.com/support/boldgrid-crio/customizing-your-blog-page-in-boldgrid-crio/" target="_blank"><span class="dashicons"></span>Help</a></div></div>',
 		'panel' => 'bgtfw_blog_panel',
+		'icon' => 'dashicons-media-document',
 	),
 	'bgtfw_pages_panel' => array(
 		'title' => __( 'Pages', 'bgtfw' ),
 		'description' => '<div class="bgtfw-description"><p>' . esc_html__( 'Change the display of your site\'s pages', 'bgtfw' ) . '</p><div class="help"><a href="https://www.boldgrid.com/support/boldgrid-crio/changing-the-page-layout-in-boldgrid-crio/" target="_blank"><span class="dashicons"></span>Help</a></div></div>',
 		'priority' => 2,
 		'panel' => 'bgtfw_design_panel',
+		'icon' => 'dashicons-admin-page',
 	),
 	'bgtfw_menus_panel' => array(
 		'title' => __( 'Menus', 'bgtfw' ),
 		'description' => '<div class="bgtfw-description"><p>' . esc_html__( 'Manage the display of menus on your site.', 'bgtfw' ) . '</p><div class="help"><a href="https://www.boldgrid.com/support/boldgrid-crio/working-with-menus-in-boldgrid-crio/" target="_blank"><span class="dashicons"></span>Help</a></div></div>',
 		'priority' => 4,
 		'panel' => 'bgtfw_design_panel',
+		'icon' => 'dashicons-menu',
 	),
 	'bgtfw_blog_posts_panel' => array(
 		'title' => __( 'Posts', 'bgtfw' ),
@@ -72,5 +80,6 @@ return array(
 			'url' => esc_url( 'https://boldgrid.com/crio/pro/blog-posts' ),
 			'buttonText' => esc_html__( 'Learn More', 'bgtfw' ),
 		],
+		'icon' => 'dashicons-admin-post',
 	),
 );

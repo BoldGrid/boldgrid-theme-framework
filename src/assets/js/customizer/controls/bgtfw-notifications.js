45f42d801db0ab1d587b4ef515d4ccefc24b43b1
Tim <dev@tim.ph>
2019-02-20 05:39:08 -0500

    reduce common logic

--- src/assets/js/customizer/controls/bgtfw-notifications.js
+++ src/assets/js/customizer/controls/bgtfw-notifications.js
@@ -1,27 +1,16 @@
-
 const api = wp.customize;
 
-export default function() {
-
-	api.bind( 'ready', function() {
-		api.previewer.bind( 'ready', function() {
-			const notices = [];
-
-			api.section.each( section => {
-				if ( ! _.isUndefined( section.params.notice ) ) {
-					notices.push( { type: 'section', data: section } );
-				}
-			} );
-
-			api.panel.each( panel => {
-				if ( ! _.isUndefined( panel.params.notice ) ) {
-					notices.push( { type: 'panel', data: panel } );
-				}
-			} );
-
-			_.each( notices, notice => {
-				api[ notice.type ]( notice.data.id ).notifications.add( `bgtfw-notice-${ notice.type }-${ notice.data.id }`, new api.Notification( `bgtfw-notice-${ notice.type }-${ notice.data.id }`, notice.data.params.notice ) );
+export default () => {
+	api.bind( 'ready', () => {
+		api.previewer.bind( 'ready', () => {
+			const types = [ 'section', 'panel' ];
+			types.map( type => {
+				api[ type ].each( item => {
+					if ( ! _.isUndefined( item.params.notice ) ) {
+						api[ type ]( item.id ).notifications.add( `bgtfw-notice-${ type }-${ item.id }`, new api.Notification( `bgtfw-notice-${ type }-${ item.id }`, item.params.notice ) );
+					}
+				} );
 			} );
 		} );
 	} );
-}
+};

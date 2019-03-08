9d4448c9b1133cd94845ec93d67a3a47ce871c97
Tim <dev@tim.ph>
2019-03-07 22:57:39 -0500

    remove external sources

--- src/includes/configs/tgm.config.php
+++ src/includes/configs/tgm.config.php
@@ -25,20 +25,8 @@ return array(
 			'required'  => true,
 		),
 		array(
-			'name'      => 'BoldGrid Post and Page Builder Premium',
-			'slug'      => 'post-and-page-builder-premium',
-			'source'    => 'https://repo.boldgrid.com/post-and-page-builder-premium.zip',
-			'required'  => true,
-		),
-		array(
 			'name'      => 'BoldGrid Backup',
 			'slug'      => 'boldgrid-backup',
-			'required'  => false,
-		),
-		array(
-			'name'      => 'BoldGrid Backup Premium',
-			'slug'      => 'boldgrid-backup-premium',
-			'source'    => 'https://repo.boldgrid.com/boldgrid-backup-premium.zip',
 			'required'  => false,
 		),
 		array(

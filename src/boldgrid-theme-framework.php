668c9403c9fbaf37ef7f08f902bb514e3002d0a7
Tim <dev@tim.ph>
2019-03-21 15:26:05 -0400

    add update license to file header and disclaimer

--- src/boldgrid-theme-framework.php
+++ src/boldgrid-theme-framework.php
@@ -8,9 +8,24 @@
  * Author URI: https://www.boldgrid.com/
  * Text Domain: bgtfw
  * Domain Path: /languages
- * License: GPLv2 or later
+ * License: GPL-3.0-or-later
  *
  * @package Boldgrid_Theme_Framework
+ * @license GPL-3.0-or-later
+ *
+ * Copyright (C) 2019 BoldGrid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
  */
 
 /**

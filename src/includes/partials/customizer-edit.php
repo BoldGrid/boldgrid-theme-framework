9be1f4562489938ca06c2e70986d95a1f266c435
Tim <dev@tim.ph>
2019-04-02 16:52:51 -0400

    remove vars and use proper translation fn

--- src/includes/partials/customizer-edit.php
+++ src/includes/partials/customizer-edit.php
@@ -4,12 +4,8 @@
  *
  * @package Boldgrid_Theme_Framework
  */
-
-// Translatable strings for the jQuery UI dialog boxes.
-$edit_page_header   = __( 'Edit this page', 'bgtfw' );
-$edit_page_content  = __( "This section is part of an individual page of your site. The Customizer (where you are now) is for editing the header and footer or an overall setting of your site like colors and fonts. To edit this section, you'll need to leave the Customizer and go to the Page Editor.", 'bgtfw' );
 ?>
-<div id='entry-content' title='<?php echo esc_attr( $edit_page_header ) ?>' class='dialog-hidden'>
-	<?php echo esc_html( $edit_page_content ); ?>
+<div id="entry-content" title="<?php esc_attr_e( 'Edit this page', 'bgtfw' ) ?>" class="dialog-hidden">
+	<?php esc_html_e( 'This section is part of an individual page of your site. The Customizer is for editing the header and footer or an overall setting of your site like colors and fonts. To edit this section, you\'ll need to leave the Customizer and go to the Page Editor.', 'bgtfw' ); ?>
 </div>
-<div id='target-highlight'></div>
+<div id="target-highlight"></div>

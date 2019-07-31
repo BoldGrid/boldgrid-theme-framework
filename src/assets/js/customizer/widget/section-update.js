8f2c96475dbf1e7067a736f748dc9c0de1e8f46b
Tim <dev@tim.ph>
2019-07-31 16:08:24 -0400

    dynamic sidebar updates should check for primary sidebar being active - fixes #118

--- src/assets/js/customizer/widget/section-update.js
+++ src/assets/js/customizer/widget/section-update.js
@@ -11,7 +11,12 @@ export default function() {
 				sections = _.filter( window._wpCustomizeSettings.sections, ( section, id ) => id.includes( 'sidebar-widgets' ) );
 
 			sections.map( section => api.section( section.id ).active.set( activeSections.includes( section.id ) ) );
+
+			// Check for primary sidebar activation/deactivation.
+			let body = $( wp.customize.previewer.container ).find( 'iframe' ).last().contents().find( 'body' )[0];
+			api.section( 'sidebar-widgets-primary-sidebar' ).active.set( body.classList.contains( 'has-sidebar' ) );
 		};
+
 		api.previewer.bind( 'ready', () => update() );
 		api.previewer.bind( 'bgtfw-widget-section-update', () => update() );
 	} );

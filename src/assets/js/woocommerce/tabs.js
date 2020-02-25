7ffaf5d3e91b76335a65774e598ce8d50aec6e41
joe9663 <joe9663@gmail.com>
2020-02-25 15:29:07 -0500

    reworks tabs.js to address dark netural color selections

---
+++ src/assets/js/woocommerce/tabs.js
@@ -0,0 +1,14 @@
+jQuery( document ).ready( function( $ ) {
+	$( 'body' ).on( 'click', '.wc-tabs li a, ul.tabs li a', function( e ) {
+		var $tab, $tabs_wrapper, $tabs;
+		e.preventDefault();
+		$tab = $( this );
+		$tabs_wrapper = $tab.closest( '.wc-tabs-wrapper, .woocommerce-tabs' );
+		$tabs = $tabs_wrapper.find( '.wc-tabs, ul.tabs' );
+        $tabs.find( 'li' ).removeClass( 'color1-background' );
+        $tabs.find( 'li > a' ).removeClass( 'color-1-text-contrast' );
+		$tabs_wrapper.find( '.wc-tab, .panel:not(.panel .panel)' ).hide();
+        $tab.closest( 'li' ).addClass( 'color1-background' );
+        $tab.closest( 'li > a' ).addClass( 'color-1-text-contrast' );
+	});
+});

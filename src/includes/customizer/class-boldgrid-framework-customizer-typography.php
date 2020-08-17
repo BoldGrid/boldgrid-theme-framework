3d7b949c4d285b429b048624041734c3070a365b
James Rosado <jamesros@boldgrid.com>
2020-08-17 12:06:14 -0400

    create kirki styles.css manually

--- src/includes/customizer/class-boldgrid-framework-customizer-typography.php
+++ src/includes/customizer/class-boldgrid-framework-customizer-typography.php
@@ -172,6 +172,14 @@ class Boldgrid_Framework_Customizer_Typography {
 	public function override_kirki_styles() {
 		$kirki_styles    = apply_filters( 'kirki_bgtfw_dynamic_css', Kirki_Modules_CSS::loop_controls( 'bgtfw' ) );
 		$filtered_styles = preg_replace( '/font-family:(\w+(\s\w+)+);/', 'font-family:"${1}";', $kirki_styles );
+		/*
+		 * Kirki no longer creates the kirki-css/styles.css file including their dynamic css.
+		 * Therefore we must create it for them in order to use the styles in the editor.
+		 */
+		if( ! is_dir( wp_upload_dir()['basedir'] . '/kirki-css' ) ) {
+			mkdir( wp_upload_dir()['basedir'] . '/kirki-css' );
+		}
+		file_put_contents( wp_upload_dir()['basedir'] . '/kirki-css/styles.css', $filtered_styles );
 		Boldgrid_Framework_Customizer_Generic::add_inline_style( 'boldgrid-kirki-override', $filtered_styles );
 	}
 

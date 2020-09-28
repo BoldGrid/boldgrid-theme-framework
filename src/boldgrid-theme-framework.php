c7ceea492579a483482f2df6be6dd86ee4e16d5b
James Rosado <jamesros@boldgrid.com>
2020-09-28 15:40:40 -0400

    use request ui instead of current screen

--- src/boldgrid-theme-framework.php
+++ src/boldgrid-theme-framework.php
@@ -3,7 +3,7 @@
  * Plugin Name: BoldGrid Theme Framework
  * Plugin URI: https://www.boldgrid.com/docs/configuration-file
  * Description: BoldGrid Theme Framework is a library that allows you to easily make BoldGrid themes. Please see our reference guide for more information: https://www.boldgrid.com/docs/configuration-file
- * Version: 1.7.6-rc.1
+ * Version: 1.7.6-rc.3
  * Author: BoldGrid.com <wpb@boldgrid.com>
  * Author URI: https://www.boldgrid.com/
  * Text Domain: bgtfw
@@ -32,8 +32,11 @@ if ( file_exists( $theme_framework_class ) ) {
 	 */
 	require_once $theme_framework_class;
 
+	$uri           = $_SERVER['REQUEST_URI'];
+	$is_admin_ajax = false !== strpos( $uri, 'admin-ajax' );
+	$is_editor     = false !== strpos( $uri, 'edit' );
 	// Only Load theme if page is admin, customizer or editor
-	if( false === is_admin() || is_customize_preview() || $is_editor ) {
+	if( false === is_admin() || is_customize_preview() || $is_admin_ajax || $is_editor ) {
 		$boldgrid_theme_framework = new Boldgrid_Framework();
 		$boldgrid_theme_framework->run();
 	}

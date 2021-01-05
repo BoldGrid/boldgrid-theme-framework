dda5bca6acf639bd082fe558872f9d084d4b4af7
James Rosado <jamesros@boldgrid.com>
2021-01-05 13:45:55 -0500

    change card contents

--- src/includes/configs/pro-feature-cards.config.php
+++ src/includes/configs/pro-feature-cards.config.php
@@ -15,14 +15,14 @@ return array(
 		'learn-more' => 'https://www.boldgrid.com/support/boldgrid-crio-supertheme-product-guide/custom-header-templates/',
 		'video'    	 => 'https://www.youtube.com/playlist?list=PLqNQzP6vlTnwl36WwXSMw6FKy6eCBHY3S',
 	),
-	'sticky-headers'      => array(
-		'title'      => __( 'Sticky Headers', 'bgtfw' ),
-		'subtitle'   => __( 'Fixed Header that are always visible.', 'bgtfw' ),
-		'learn-more' => 'https://www.boldgrid.com/support/boldgrid-crio-supertheme-product-guide/customizing-the-header-design-in-boldgrid-crio/',
-	),
 	'white-labeling'      => array(
 		'title'      => __( 'White Labeling', 'bgtfw' ),
 		'subtitle'   => __( 'Remove Attribution Links.', 'bgtfw' ),
 		'learn-more' => 'https://www.boldgrid.com/support/boldgrid-crio-supertheme-product-guide/customizing-the-footer-design-in-boldgrid-crio/',
+	),
+	'more-customization'      => array(
+		'title'      => __( 'More Customization', 'bgtfw' ),
+		'subtitle'   => __( '150+ Additional Customizer Controls.', 'bgtfw' ),
+		'learn-more' => 'https://www.boldgrid.com/support/boldgrid-crio-supertheme-product-guide/customizing-the-header-design-in-boldgrid-crio/',
 	),
 );

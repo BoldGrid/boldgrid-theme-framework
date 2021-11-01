c6ec0490e2b7dd923363a3bcf2d2905ce7cf1ddb
James Rosado <jamesros@boldgrid.com>
2021-11-01 16:15:29 -0400

    fix page title metabox

--- src/includes/class-boldgrid-framework-title.php
+++ src/includes/class-boldgrid-framework-title.php
@@ -97,14 +97,15 @@ class Boldgrid_Framework_Title {
 		}
 
 		$post_meta = get_post_meta( $post->ID, $this->configs['title']['hide'], true );
-		$title = sprintf( '%1$s %2$s', 'post' === $post->post_type ? __( 'Post', 'bgtfw' ) : __( 'Page', 'bgtfw' ), __( 'Title', 'bgtfw' ) );
+		$title     = sprintf( '%1$s %2$s', 'post' === $post->post_type ? __( 'Post', 'bgtfw' ) : __( 'Page', 'bgtfw' ), __( 'Title', 'bgtfw' ) );
+		$k         = $this->get_global();
 
 		$options = array(
 			'global' => array(
-				'name' => __( 'Use Global Settings', 'bgtfw' ),
+				'name' => __( 'Use Global Settings ', 'bgtfw' ),
 				'value' => 'global',
 				'checked' => 'global' === $post_meta,
-				'post_text' => $this->configs['title']['meta_box'][ $post->post_type ]['global_post_text'],
+				'post_text' => esc_attr( $k ),
 			),
 			'show' => array(
 				'name' => __( 'Show', 'bgtfw' ),
@@ -144,7 +145,14 @@ class Boldgrid_Framework_Title {
 
 		?>
 		<div class="misc-pub-section bgtfw-misc-pub-section bgtfw-page-title">
-			<?php echo esc_html( $title ); ?>: <span class="value-displayed">...</span>
+			<?php
+				if ( $options['global']['checked'] ) {
+					$k = get_theme_mod( 'bgtfw_pages_title_display' );
+					echo esc_html( $title ) . ': <span class="value-displayed">' . esc_html__( 'Use Global Settings ', 'bgtfw' ) . '<div class="template-subtitle">' . esc_html( $k ) . '</div></span>';
+				} else {
+					echo esc_html( $title ) . '<span class="value-displayed">...</span>';
+				}
+			?>
 			<a class="edit" href="">
 				<span aria-hidden="true"><?php esc_html_e( 'Edit', 'bgtfw' ); ?></span> <span class="screen-reader-text"><?php echo esc_html( $title ); ?></span>
 			</a>

44a930b4de55e737573bfae73e7e42a3ce304a36
jamesros <jamesros@boldgrid.com>
2022-01-07 13:19:45 -0500

    apply correct typography classes (#591)
    * apply correct typography classes
    
    * fix arrow alignments

--- src/includes/configs/customizer-options/typography.config.php
+++ src/includes/configs/customizer-options/typography.config.php
@@ -11,71 +11,66 @@
  */
 
 return array(
-	'enabled' => false,
-	'controls' => array(
-		'site_title'     => true,
-		'headings' => true,
+	'enabled'                  => false,
+	'controls'                 => array(
+		'site_title'         => true,
+		'headings'           => true,
 		'alternate_headings' => true,
-		'main_text' => true,
-		'navigation' => true,
+		'main_text'          => true,
+		'navigation'         => true,
 	),
-	'defaults' => array(
-		'headings_font_size' => '18px',
-		'headings_font_family' => 'Roboto',
-		'headings_text_transform' => 'uppercase',
-		'alternate_headings_font_size' => 14,
-		'alternate_headings_font_family' => 'Oswald',
+	'defaults'                 => array(
+		'headings_font_size'                => '18px',
+		'headings_font_family'              => 'Roboto',
+		'headings_text_transform'           => 'uppercase',
+		'alternate_headings_font_size'      => 14,
+		'alternate_headings_font_family'    => 'Oswald',
 		'alternate_headings_text_transform' => 'uppercase',
-		'body_font_size' => '18px',
-		'body_font_family' => 'Roboto',
-		'body_line_height' => '1.4',
-		'navigation_font_size' => '18px',
-		'navigation_text_transform' => 'uppercase',
-		'navigation_font_family' => 'Roboto',
+		'body_font_size'                    => '18px',
+		'body_font_family'                  => 'Roboto',
+		'body_line_height'                  => '1.4',
+		'navigation_font_size'              => '18px',
+		'navigation_text_transform'         => 'uppercase',
+		'navigation_font_family'            => 'Roboto',
 	),
-	'selectors' => array(
+	'selectors'                => array(
 		'h1, .h1' => array(
-			'type' => 'headings',
-			'round' => 'floor',
+			'type'   => 'headings',
+			'round'  => 'floor',
 			'amount' => 2.6,
 		),
 		'h2, .h2' => array(
-			'type' => 'headings',
-			'round' => 'floor',
+			'type'   => 'headings',
+			'round'  => 'floor',
 			'amount' => 2.15,
 		),
 		'h3, .h3' => array(
-			'type' => 'headings',
-			'round' => 'ceil',
+			'type'   => 'headings',
+			'round'  => 'ceil',
 			'amount' => 1.7,
 		),
 		'h4, .h4' => array(
-			'type' => 'headings',
-			'round' => 'ceil',
+			'type'   => 'headings',
+			'round'  => 'ceil',
 			'amount' => 1.25,
 		),
 		'h5, .h5' => array(
-			'type' => 'headings',
-			'round' => 'floor',
+			'type'   => 'headings',
+			'round'  => 'floor',
 			'amount' => 1,
 		),
 		'h6, .h6' => array(
-			'type' => 'headings',
-			'round' => 'ceil',
+			'type'   => 'headings',
+			'round'  => 'ceil',
 			'amount' => 0.85,
 		),
 		// Enable this styling for Page Header Headings.
-		'.bgc-heading.bgc-page-title' => array(
-			'type' => 'headings',
-			'round' => 'floor',
-			'amount' => 1,
-		),
 		'.widget, .site-content, .attribution-theme-mods-wrapper, .gutenberg .edit-post-visual-editor, .mce-content-body, .template-header' => array(
 			'type' => 'body',
 		),
 	),
 	'responsive_font_controls' => array(
-		'bgtfw_body_font_size' => array(
+		'bgtfw_body_font_size'                => array(
 			'section'         => 'boldgrid_typography',
 			'priority'        => 2,
 			'output_selector' => '.widget, .site-content, .attribution-theme-mods-wrapper, .gutenberg .edit-post-visual-editor, .mce-content-body, .template-header',
@@ -83,12 +78,17 @@ return array(
 		'bgtfw_headings_responsive_font_size' => array(
 			'section'         => 'boldgrid_typography',
 			'priority'        => 5,
-			'output_selector' => 'h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6, .bgc-heading.bgc-page-title',
+			'output_selector' => 'h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6',
 		),
-		'bgtfw_site_title_font_size' => array(
+		'bgtfw_site_title_font_size'          => array(
 			'section'         => 'bgtfw_site_title',
 			'priority'        => 21,
 			'output_selector' => '.site-footer .site-title > a, .' . get_theme_mod( 'boldgrid_palette_class', 'palette-primary' ) . '.site-header .site-title > a, .' . get_theme_mod( 'boldgrid_palette_class', 'palette-primary' ) . ' .site-header .site-title > a,.' . get_theme_mod( 'boldgrid_palette_class', 'palette-primary' ) . ' .site-header .site-title > a:hover, .bgc-site-title, .bgc-site-title:hover',
+		),
+		'bgtfw_site_tagline_font_size'        => array(
+			'section'         => 'bgtfw_tagline',
+			'priority'        => 21,
+			'output_selector' => '.site-branding .site-description, .bgc-tagline',
 		),
 	),
 );

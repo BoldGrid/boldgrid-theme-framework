c2616f13e090fe81ec2e262eb54be29fc065935d
James Rosado <jamesros@boldgrid.com>
2022-02-21 13:05:24 -0500

    remove max-width controls for now

--- src/includes/configs/customizer/controls/blog-page.controls.php
+++ src/includes/configs/customizer/controls/blog-page.controls.php
@@ -45,47 +45,6 @@ return array(
 			return 'container' === $value || '' === $value ? $value : $settings->default;
 		},
 	),
-	'bgtfw_blog_page_container_max_width'              => array(
-		'type'              => 'kirki-generic',
-		'transport'         => 'postMessage',
-		'section'           => 'bgtfw_pages_blog_blog_page_post_content',
-		'settings'          => 'bgtfw_blog_page_container_max_width',
-		'label'             => 'Container Max Width',
-		'default'           => $bgtfw_generic->get_width_defaults( 'max-width' ),
-		'priority'          => 37,
-		'sanitize_callback' => array( 'Boldgrid_Framework_Customizer_Generic', 'sanitize' ),
-		'choices'           => array(
-			'name'     => 'boldgrid_controls',
-			'type'     => 'ContainerWidth',
-			'settings' => array(
-				'responsive' => array(
-					'tablet'  => 991,
-					'desktop' => 1199,
-				),
-				'control'    => array(
-					'selectors' => array(
-						'body.blog .container-fluid',
-						'body.archive .container-fluid',
-						'body.blog .boldgrid-section > .full-width',
-						'body.archive .boldgrid-section > .full-width',
-					),
-					'title'     => 'Container Max Width',
-					'name'      => 'bgtfw_blog_page_container_max_width',
-					'sliders'   => array(
-						array(
-							'name'        => 'maxWidth',
-							'label'       => '',
-							'cssProperty' => 'max-width',
-						),
-					),
-					'units'     => array(
-						'default' => '%',
-						'enabled' => array( 'px', '%' ),
-					),
-				),
-			),
-		),
-	),
 	'bgtfw_pages_blog_blog_page_layout_posts_per_page' => array(
 		'label'       => __( 'Blog Posts Per Page', 'bgtfw' ),
 		'tooltip'     => __( 'Set how many posts display per page for your blog, categories, archives, and search pages.', 'bgtfw' ),

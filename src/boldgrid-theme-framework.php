05faf3f5c89aebb0ef9f1e29405e2524705601cd
jamesros <jamesros@boldgrid.com>
2022-08-11 09:23:52 -0400

    Branch 2.16.2 (#720)
    * update versions and readme for 2.16.2
    
    * resolves #708 (#719)
    
    * resolves #708
    
    * simplify ternary+
    
    * resolves #363 (#718)
    
    * update version numbers for stable release

--- src/boldgrid-theme-framework.php
+++ src/boldgrid-theme-framework.php
@@ -3,7 +3,7 @@
  * Plugin Name: BoldGrid Theme Framework
  * Plugin URI: https://www.boldgrid.com/docs/configuration-file
  * Description: BoldGrid Theme Framework is a library that allows you to easily make BoldGrid themes. Please see our reference guide for more information: https://www.boldgrid.com/docs/configuration-file
- * Version: 2.16.1
+ * Version: 2.16.2
  * Author: BoldGrid.com <wpb@boldgrid.com>
  * Author URI: https://www.boldgrid.com/
  * Text Domain: bgtfw
